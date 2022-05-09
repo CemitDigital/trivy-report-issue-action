3f05802494a2484100e636e58c2846e37b7e08b8
James Luther <81767471+b34rd-tek@users.noreply.github.com>
2022-05-09 10:46:34 -0500

    Update report_issues.py

--- trivy_report/report_issues.py
+++ trivy_report/report_issues.py
@@ -75,7 +75,7 @@ def main():
     try:
         reports = parse_results(data, existing_issues=existing_issues)
     except TypeError as e:
-        if e = "The JSON entry does not contain Results key":
+        if e == "The JSON entry does not contain Results key":
             pass
         else:
             abort(f"Failed to parse Trivy JSON report: {e}")
